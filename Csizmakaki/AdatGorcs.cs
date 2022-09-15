3d3fe1c3ad1b5e2882addbbd0d1f37b9cdfee7ac
bnhj <usscommando@gmail.com>
2022-09-15 12:10:00 +0200

    Add project files.

---
+++ Csizmakaki/AdatGorcs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csizmakaki
+{
+    class AdatGorcs
+    {
+        private int elso;
+
+        public int Elso
+        {
+            get { return elso; }
+            set { elso = value; }
+        }
+
+        private string masodik;
+
+        public string Masodik
+        {
+            get { return masodik; }
+            set { masodik = value; }
+        }
+
+        private int harmadik;
+
+        public int Harmadik
+        {
+            get { return harmadik; }
+            set { harmadik = value; }
+        }
+
+        public AdatGorcs(string a)
+        {
+            string[] temp = a.Split();
+            elso = Convert.ToInt32(temp[0]);
+            masodik = temp[1];
+            harmadik = Convert.ToInt32(temp[2]);
+
+            Elso = elso;
+            Masodik = masodik;
+            Harmadik = harmadik;
+
+        }
+
+    }
+}
---
+++ Csizmakaki/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Csizmakaki
+{
+
+    class Program
+    {
+        static List<AdatGorcs> adatok = new List<AdatGorcs>();
+
+        static void Main(string[] args)
+        {
+            ElsoFeladat();
+            MasodikFeladat();
+
+        }
+
+        private static void MasodikFeladat()
+        {
+            Console.WriteLine($"Az állományban {adatok.Count} kifejezés van");
+        }
+
+        private static void ElsoFeladat()
+        {
+            using (StreamReader sr = new StreamReader("kifejezesek.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    adatok.Add(new AdatGorcs(sr.ReadLine()));
+                }
+            }
+        }
+    }
+}
