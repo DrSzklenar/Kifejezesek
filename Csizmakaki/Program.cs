682825faa3ed5dcd9dc7bd1286967d6094b0c9a5
a név minek <usscommando@gmail.com>
2022-09-18 19:30:21 +0200

    Kész az egész

--- Csizmakaki/Program.cs
+++ Csizmakaki/Program.cs
@@ -19,16 +19,87 @@ namespace Csizmakaki
             HarmadikFeladat();
             NegyedikFeladat();
             OtodikFeladat();
-
+            HetedikFeladat();
+            NyolcasFeladat();
             Console.ReadKey();
+        }
+
+        private static void NyolcasFeladat()
+        {
+            StreamWriter sw = new StreamWriter("Eredmenyek.txt");
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                sw.WriteLine($"{adatok[i].Elso} {adatok[i].Masodik} {adatok[i].Harmadik} =  {Szamitas(Convert.ToDouble(adatok[i].Elso), adatok[i].Masodik, Convert.ToDouble(adatok[i].Harmadik))}");
+
+            }
+            sw.Close();
+        }
+
+        private static string Szamitas(double v1, string v2, double v3)
+        {
+            string temp = "";
+            switch (v2)
+            {
+                case "+":
+                    return temp = Convert.ToString(v1 + v3);
+                    break;
+
+                case "-":
+                    return temp = Convert.ToString(v1 - v3);
+                    break;
+
+                case "*":
+                    return temp = Convert.ToString(v1 * v3);
+                    break;
+
+                case "/":
+                    return temp = Convert.ToString(v1 / v3);
+                    break;
+
+                case "div":
+                    return temp = Convert.ToString(Math.Round(v1 / v3));
+                    break;
+
+                case "mod":
+                    return temp = Convert.ToString(v1 / v3);
+                    break;
+
+
+
+                default:
+                    return "hibás operátor!";
+                    break;
+            }
+        }
+
+        private static void HetedikFeladat()
+        {
+            Console.WriteLine("Írjon be egy kifejezést");
+            string[] geccccccooo = Console.ReadLine().Split();
+            Console.WriteLine(Szamitas(Convert.ToDouble(geccccccooo[0]), geccccccooo[1], Convert.ToDouble(geccccccooo[2])));
         }
 
         private static void OtodikFeladat()
         {
-            Dictionary<string, int> hang = new Dictionary<string, int>();
+            Dictionary<string, int> statisztika  = new Dictionary<string, int>();
             for (int i = 0; i < adatok.Count; i++)
             {
-                
+                if (adatok[i].Masodik == "div" || adatok[i].Masodik == "mod" || adatok[i].Masodik == "+" || adatok[i].Masodik == "-" || adatok[i].Masodik == "*" || adatok[i].Masodik == "/")
+                {
+                    if (!statisztika.ContainsKey(adatok[i].Masodik))
+                    {
+
+                        statisztika.Add(adatok[i].Masodik, 1);
+                    }
+                    else
+                    {
+                        statisztika[adatok[i].Masodik]++;
+                    }
+                }
+            }
+            foreach (var i in statisztika)
+            {
+                Console.WriteLine($"\t{i.Key} -> {i.Value}");
             }
         }
 
